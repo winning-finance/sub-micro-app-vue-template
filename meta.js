ca04242fea9ee0d26b864f695b05159899257257
qiangfeng <qiangfeng@winning.com.cn>
2020-09-15 10:16:49 +0800

    add auto install

--- meta.js
+++ meta.js
@@ -1,9 +1,7 @@
 const path = require('path')
 
 const {
-  sortDependencies,
   installDependencies,
-  runLintFix,
   printMessage
 } = require('./utils')
 const pkg = require('./package.json')
@@ -71,6 +69,12 @@ module.exports = {
       type: 'number',
       message: 'webpack-dev-server port?',
       default: '3000'
+    },
+    autoInstall: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'auto install dependencies?',
+      default: true
     }
   },
   filters: {
@@ -78,25 +82,17 @@ module.exports = {
     'src/store/**/*': 'store'
   },
   complete: function (data, { chalk }) {
-    // const green = chalk.green
-
-    // sortDependencies(data, green)
-
-    // const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
-
-    // if (data.autoInstall) {
-    //   installDependencies(cwd, data.autoInstall, green)
-    //     .then(() => {
-    //       return runLintFix(cwd, data, green)
-    //     })
-    //     .then(() => {
-    //       printMessage(data, green)
-    //     })
-    //     .catch(e => {
-    //       console.log(chalk.red('Error:'), e)
-    //     })
-    // } else {
-    printMessage(data, chalk)
-    // }
+    const green = chalk.green
+    if (data.autoInstall) {
+      installDependencies(process.cwd(), green)
+        .then(() => {
+          printMessage(data, green)
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e)
+        })
+    } else {
+      printMessage(data, chalk)
+    }
   }
 }
